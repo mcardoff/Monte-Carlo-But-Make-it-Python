c565b06ef4f8970fe469b37ed77d95eebb1d8e94
Michael Cardiff <mcardiff0321@gmail.com>
2022-02-20 15:36:50 -0600

    Remove wavefuncs from main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from wavefunctions import *
 from enum import Enum
 
 if __name__ == "__main__":
@@ -67,42 +68,6 @@ class MonteCarlo():
 
     def calculateAvg(self):
         return sum(self.valList)/len(self.valList)
-
-def psi1s(x,y,z):
-    r = np.sqrt(x*x+y*y+z*z)
-    theta = np.arccos(z/r)
-    phi = convertPhi(x,y,z)
-    return psi1sSpherical(r,theta,phi)
-
-def psi1sSpherical(r,theta,phi):
-    coeff = 1 / np.sqrt(np.pi)
-    expon = np.exp(-r)
-    return coeff * expon
-
-def psi2px(x,y,z):
-    r = np.sqrt(x*x+y*y+z*z)
-    theta = np.arccos(z/r)
-    phi = convertPhi(x,y,z)
-    return psi2pxSpherical(r,theta,phi)
-
-def psi2pxSpherical(r,theta,phi):
-    coeff = (1 / (4 * np.sqrt(2 * np.pi))) * r * np.sin(theta) * np.cos(phi)
-    expon = np.exp(-r / 2)
-    return coeff * expon
-
-def convertPhi(x,y,z):
-    if x > 0.0:
-        return np.arctan(y/x)
-    elif x < 0.0 and y >= 0.0:
-        return np.arctan(y/x) + np.pi
-    elif x < 0.0 and y < 0.0:
-        return np.arctan(y/x) - np.pi
-    elif x == 0.0 and y > 0:
-        return np.pi / 2
-    elif x == 0.0 and y < 0:
-        return - np.pi/2
-    else:
-        return -1
 
 def visualize(funVals, coords):
     rLayer1 = []
